d086b3fab839aa067b3236a24e7180ac2bc3f763
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-12-29 18:12:22 +0700

    ENHANCEMENT: Addition of geo

--- code/FlickrBucket.php
+++ code/FlickrBucket.php
@@ -10,7 +10,8 @@ class FlickrBucket extends DataObject {
     'Lat' => 'Decimal(18,15)',
     'Lon' => 'Decimal(18,15)',
     'Accuracy' => 'Int',
-    'ZoomLevel' => 'Int'
+    'ZoomLevel' => 'Int',
+    'TagsCSV' => 'Varchar'
   );
 
 
@@ -54,7 +55,28 @@ class FlickrBucket extends DataObject {
         $fields->push($lf2);
 
 
-         // only show a map for editing if no sets have geolock on them
+        $lockgeo = $this->GeoLocked();
+    
+        if (!$lockgeo) {
+          error_log("Adding location tab as lock geo is ".$lockgeo);
+           $fields->addFieldToTab( "Root.Location", new LatLongField( array(
+              new TextField( 'Lat', 'Latitude' ),
+              new TextField( 'Lon', 'Longitude' ),
+              new TextField( 'ZoomLevel', 'Zoom' )
+            ),
+            array( 'Address' )
+          ) );
+        }
+   
+
+
+       
+        return $fields;
+    }
+
+
+    function GeoLocked() {
+      // only show a map for editing if no sets have geolock on them
         $lockgeo = false;
         foreach ($this->FlickrPhotos() as $fp) {
           foreach ($fp->FlickrSets() as $set) {
@@ -69,32 +91,18 @@ class FlickrBucket extends DataObject {
             break;
           }
         }
-    
 
-    error_log("++++ LOCK GEO: BUCKET - ".$lockgeo);  
-
-    if (!$lockgeo) {
-      error_log("Adding location tab as lock geo is ".$lockgeo);
-       $fields->addFieldToTab( "Root.Location", new LatLongField( array(
-          new TextField( 'Lat', 'Latitude' ),
-          new TextField( 'Lon', 'Longitude' ),
-          new TextField( 'ZoomLevel', 'Zoom' )
-        ),
-        array( 'Address' )
-      ) );
-    }
-   
-
-
-       
-        return $fields;
+        return $lockgeo;
     }
 
 
     public function getImageStrip() {
       $html = '<div class="imageStrip">';
       foreach ($this->FlickrPhotos() as $key => $photo) {
-        $html = $html . '<img src="'.$photo->ThumbnailURL.'"/>';
+        $html = $html . '<img class="flickrThumbnail" ';
+        $html .= 'src="'.$photo->ThumbnailURL.'" ';
+        $html .= 'data-flickr-thumbnail-url="'.$photo->ThumbnailURL.'" ';
+        $html .= 'data-flickr-medium-url="'.$photo->MediumURL.'"/>';
       }
       $html = $html . "</div>";
       return DBField::create_field( 'HTMLText',  $html);
@@ -103,9 +111,47 @@ class FlickrBucket extends DataObject {
 
     public function onBeforeWrite() {
       parent::onBeforeWrite();
-      if ($this->Title == '') {
-        error_log("Blank title");
-        $this->Title = $this->FlickrPhotos()->first()->TakenAt.' - '.$this->FlickrPhotos()->last()->TakenAt;
+
+
+      error_log("OBW: ID = ".$this->ID);
+
+      if ($this->ID && ($this->FlickrPhotos()->count() > 0)) {
+        error_log("TESTING TITLE");
+        if ($this->Title == '') {
+          error_log("Blank title nFP=".$this->FlickrPhotos()->count());
+          $this->Title = $this->FlickrPhotos()->first()->TakenAt.' - '.$this->FlickrPhotos()->last()->TakenAt;
+        }
+      } else {
+        error_log("SKIPPING TITLE TWEAK");
+        $this->Virginal = true;
+      }
+      
+    }
+
+
+    /*
+    Update all the photographs in the bucket with the details of the bucket
+    */
+    public function onAfterWrite() {
+      parent::onAfterWrite();
+
+      // if the title is blank resave in order to create a time from / time to title
+      // this needs checked here as on before write cannot do this when the bucket has not been saved
+      if ($this->Title == '' && !isset($this->Virginal)) {
+        $this->write();
+      }
+
+      $lockgeo = $this->GeoLocked();
+      foreach ($this->FlickrPhotos() as $fp) {
+        $fp->Title = $this->Title;
+        $fp->Description = $this->Description;
+        if ($lockgeo) {
+          if ($this->Lat != 0 &&  $this->Lon != 0)
+          $fp->Lat = $this->Lat;
+          $fp->Lon = $this->Lon;
+        }
+
+        $fp->write();
       }
     }
 
