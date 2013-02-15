247cb52efe5d8c8e82c1c30620b0a3057c755edc
Gordon Anderson <gordon.b.anderson@gmail.com>
2013-02-15 20:14:33 +0700

    ENHANCEMENT: Simple widget for pasting FlickrID to get an image

---
+++ code/FlickrPhotoSelectionField.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Text input field.
+ *
+ * @package forms
+ * @subpackage fields-basic
+ */
+class FlickrPhotoSelectionField extends HiddenField {
+
+	/**
+	 * @var int
+	 */
+	protected $maxLength;
+
+	protected $flickrTitle;
+	protected $flickrID;
+
+	/**
+	 * Returns an input field, class="text" and type="text" with an optional maxlength
+	 */
+	function __construct($name, $title = null, $flickrPhoto = '', $maxLength = null, $form = null) {
+		$this->maxLength = $maxLength;
+
+		parent::setTemplate('FLickrPhotoSelectionField');
+
+		error_log("FLICKR PHOTO SELECTION FIELD");
+
+		$value = '';
+		if ($flickrPhoto) {
+			$value = $flickrPhoto->ID;
+			$this->flickrTitle = $flickrPhoto->Title;
+			$this->flickrID = $flickrPhoto->FlickrID;
+			$this->mediumURL = $flickrPhoto->MediumURL;
+		}
+
+		$this->addExtraClass('flickrPhotoSelectionField');
+
+		
+		parent::__construct($name, $title, $value, $form);
+	}
+
+	function getFlickrTitle() {
+		error_log("FLICKR TITLE:".$this->flickrTitle);
+		return $this->flickrTitle;
+	}
+
+	function getFlickrID() {
+		return $this->flickrID;
+	}
+
+	function getMediumURL() {
+		return $this->mediumURL;
+	}
+	
+	/**
+	 * @param int $length
+	 */
+	function setMaxLength($length) {
+		$this->maxLength = $length;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	function getMaxLength() {
+		return $this->maxLength;
+	}
+
+	function getAttributes() {
+		return array_merge(
+			parent::getAttributes(),
+			array(
+				'maxlength' => $this->getMaxLength(),
+				'size' => ($this->getMaxLength()) ? min($this->getMaxLength(), 30) : null
+			)
+		);
+	}
+
+	function InternallyLabelledField() {
+		if(!$this->value) $this->value = $this->Title();
+		return $this->Field();
+	}
+
+
+	public function FieldHolder( $properties = array() ) {
+		Requirements::javascript( 'weboftalent-flickr/javascript/flickredit.js' );
+
+
+		return parent::FieldHolder();
+	}
+	
+}
+
