ac55293123ec20ad350f41f34d6abc1b2065699f
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-07 14:51:15 +0700

    MINOR: Addition of menu icon

--- code/FlickrPhotoAdmin.php
+++ code/FlickrPhotoAdmin.php
@@ -1,14 +1,15 @@
+
 <?php
 
 class FlickrPhotoAdmin extends ModelAdmin {
-    
+
   public static $managed_models = array(   //since 2.3.2
       'FlickrPhoto'
    );
- 
+
   static $url_segment = 'flickr_photos'; // will be linked as /admin/products
   static $menu_title = 'Flickr Photos';
- 
-}
 
-?>
+  static $menu_icon = '/flickr/icons/photo.png';
+
+}
