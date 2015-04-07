de41d04329a1f0cc646c394bd018729049cdb05e
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-07 14:51:47 +0700

    MINOR: Addition of icon, whitespace

--- code/FlickrSetAdmin.php
+++ code/FlickrSetAdmin.php
@@ -1,14 +1,15 @@
 <?php
 
 class FlickrSetAdmin extends ModelAdmin {
-    
-  public static $managed_models = array(   //since 2.3.2
-      'FlickrSet'
-   );
- 
-  static $url_segment = 'flickr_sets'; // will be linked as /admin/products
-  static $menu_title = 'Flickr Sets';
- 
-}
 
-?>
+  public static $managed_models = array(   //since 2.3.2
+	  'FlickrSet'
+   );
+
+	static $url_segment = 'flickr_sets'; // will be linked as /admin/products
+	static $menu_title = 'Flickr Sets';
+
+	static $menu_icon = '/flickr/icons/album.png';
+
+
+}
