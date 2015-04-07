442da472d03279b0cb774ced8449209f140d2ecb
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-07 14:51:59 +0700

    MINOR: Addition of icon, whitespace

--- code/FlickrSetPageFolderAdmin.php
+++ code/FlickrSetPageFolderAdmin.php
@@ -7,28 +7,30 @@
 class FlickrSetPageFolderAdmin extends ModelAdmin {
     static $url_segment = 'flickrsetpagefolders';
 	static $menu_title = 'Flickr Set Folders';
-	
+
 	static $managed_models = array('FlickrSetPage');
 
-	
+	static $menu_icon = '/flickr/icons/folders.png';
+
+
 
 	/**
 	 *
 	 * @var QueuedJobService
 	 */
 	public $jobQueue;
-	
+
 	public function EditForm($request = null) {
 		$form = parent::EditForm($request);
 
 		Requirements::javascript( FLICKR_EDIT_TOOLS_PATH . '/javascript/flickredit.js' );
     	Requirements::css( FLICKR_EDIT_TOOLS_PATH . '/css/flickredit.css' );
-		
+
 
 		$flickrSetPages = DataList::create('FlickrSetPage')->where('ParentID = 0');//->sort('Title desc');
 		$flickrSetFolders = DataList::create('FlickrSetFolder')->sort('Title');
 
-		
+
 
 
 		$html ='<h2>Flickr Set Folders</h2><div id="flickrFolders">';
@@ -49,10 +51,10 @@ class FlickrSetPageFolderAdmin extends ModelAdmin {
     	$fl = new FieldList();
     	$fl->push($lf);
     	$form->setFields($fl);
-		
+
 		return $form;
 	}
-	
+
 	public function Tools() {
 		return '';
 	}
