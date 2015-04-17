9e961111d742f6814cae96f7ff94ddd949b803ea
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-17 19:48:34 +0700

    FIX: Whitespace

--- code/FlickrSetFolder.php
+++ code/FlickrSetFolder.php
@@ -4,74 +4,71 @@
  */
 class FlickrSetFolder extends Page implements RenderableAsPortlet {
 
-   
-   static $allowed_children = array('FlickrSetPage', 'FlickrSetFolder');
 
-   static $db = array(
-    'PromoteToHomePage' => 'Boolean'
-   );
+	 static $allowed_children = array('FlickrSetPage', 'FlickrSetFolder');
 
-    
-    static $has_one = array('MainFlickrPhoto' => 'FlickrPhoto');
+	 static $db = array(
+	'PromoteToHomePage' => 'Boolean'
+	 );
 
 
-
-   function getCMSFields() {
-    $fields = parent::getCMSFields();
-    $fields->addFieldToTab('Root.CoverPhoto', new FlickrPhotoSelectionField('MainFlickrPhotoID', 'Cover Photo', $this->MainFlickrPhoto()));
-
-    
-    $fields->renameField("Content", "Brief Description");
-    $fields->addFieldToTab("Root.HomePage", new CheckboxField('PromoteToHomePage', 'Promote to Home Page'));
-
-   return $fields;
-  }
+	static $has_one = array('MainFlickrPhoto' => 'FlickrPhoto');
 
 
 
-    function getPortletTitle() {
-        return $this->Title;
-    }
-    
+	function getCMSFields() {
+		$fields = parent::getCMSFields();
+		$fields->addFieldToTab('Root.CoverPhoto', new FlickrPhotoSelectionField('MainFlickrPhotoID', 'Cover Photo', $this->MainFlickrPhoto()));
 
-    /**
-     * An accessor method for an image for a portlet
-     * @example
-     * <code>
-     *  return $this->NewsItemImage;
-     * </code>
-     *
-     * @return string
-     */
-    public function getPortletImage() {
-        return $this->MainFlickrPhoto();
-    }
-    
-    
-    /**
-     * An accessor for text associated with the portlet
-     * @example
-     * <code>
-     * return $this->Summary
-     * </code>
-     *
-     * @return string
-     */ 
-    public function getPortletCaption() {
-        return $this->Title;
-    }
 
-  
+		$fields->renameField("Content", "Brief Description");
+		$fields->addFieldToTab("Root.HomePage", new CheckboxField('PromoteToHomePage', 'Promote to Home Page'));
+
+		return $fields;
+	}
+
+
+	function getPortletTitle() {
+		return $this->Title;
+	}
+
+
+	/**
+	 * An accessor method for an image for a portlet
+	 * @example
+	 * <code>
+	 *  return $this->NewsItemImage;
+	 * </code>
+	 *
+	 * @return string
+	 */
+	public function getPortletImage() {
+		return $this->MainFlickrPhoto();
+	}
+
+
+	/**
+	 * An accessor for text associated with the portlet
+	 * @example
+	 * <code>
+	 * return $this->Summary
+	 * </code>
+	 *
+	 * @return string
+	 */
+	public function getPortletCaption() {
+		return $this->Title;
+	}
+
+
 }
- 
+
 class FlickrSetFolder_Controller extends Page_Controller {
-  public function FlickrSetsNewestFirst() {
-    return DataList::create('FlickrSetPage')->where('ParentID = '.$this->ID)->sort('FirstPictureTakenAt desc');
-  }
+	public function FlickrSetsNewestFirst() {
+		return DataList::create('FlickrSetPage')->where('ParentID = '.$this->ID)->sort('FirstPictureTakenAt desc');
+	}
 
-  public function FlickrSetFoldersNewestFirst() {
-    return DataList::create('FlickrSetFolder')->where('ParentID = '.$this->ID)->sort('Created desc');
-  }
+	public function FlickrSetFoldersNewestFirst() {
+		return DataList::create('FlickrSetFolder')->where('ParentID = '.$this->ID)->sort('Created desc');
+	}
 }
- 
-?>
