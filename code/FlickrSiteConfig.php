bec5edc23a4ca9f5e32986caacdde9671d492f8f
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-17 19:54:20 +0700

    FIX: Whitespace

--- code/FlickrSiteConfig.php
+++ code/FlickrSiteConfig.php
@@ -1,19 +1,17 @@
 <?php
-  
+
 class FlickrSiteConfig extends DataExtension {
-     
-    
-    private static $db = array(
-        'ImageFooter' => 'Text',
-        'AddLocation' => 'Boolean'
-    );
-    
-  
-    function updateCMSFields(FieldList $fields) {
-        $fields->addFieldToTab("Root.Flickr", new TextareaField("ImageFooter", 'This text will be appended to all image descriptions'));
-        $fields->addFieldToTab("Root.Flickr", new CheckboxField("AddLocation", 'Add a textual description of the location to all images'));//, 'Add the location as text to the picture');
-        return $fields;
-    }
-     
-    
+
+	private static $db = array(
+		'ImageFooter' => 'Text',
+		'AddLocation' => 'Boolean'
+	);
+
+
+	function updateCMSFields(FieldList $fields) {
+		$fields->addFieldToTab("Root.Flickr", new TextareaField("ImageFooter", 'This text will be appended to all image descriptions'));
+		$fields->addFieldToTab("Root.Flickr", new CheckboxField("AddLocation", 'Add a textual description of the location to all images'));//, 'Add the location as text to the picture');
+		return $fields;
+	}
+
 }
