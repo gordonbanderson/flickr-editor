bf98e70d3e1e4d393a4b9987663709fc44b384e8
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-17 20:00:41 +0700

    FIX: Whitespace

--- code/GridFieldFlickrImage.php
+++ code/GridFieldFlickrImage.php
@@ -1,16 +1,13 @@
 <?php
 /**
  * GridFieldExifData displays read only exif data for a Flickr photograph
- * 
+ *
  * @see GridField
- * 
+ *
  * @package weboftalent-flickr
  * @subpackage fields-relational
  */
 class GridFieldFlickrImage implements GridField_HTMLProvider {
-
-	
-
 	public function getHTMLFragments($gridField) {
 		$forTemplate = new ArrayData(array());
 
@@ -19,4 +16,3 @@ class GridFieldFlickrImage implements GridField_HTMLProvider {
 		);
 	}
 }
-?>
