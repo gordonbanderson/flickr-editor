4d1b9161374bfccaaa16e908244386dbe8666e56
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-17 19:43:53 +0700

    FIX: Whitespace

--- code/FlickrFolder.php
+++ code/FlickrFolder.php
@@ -4,33 +4,31 @@
  */
 class FlickrFolder extends Page {
 
-   
-   static $allowed_children = array('FlickrSetPage', 'FlickrFolder');
 
-   static $has_one = array(
-      'CoverPhoto' => 'Image',
-   );
+	 static $allowed_children = array('FlickrSetPage', 'FlickrFolder');
+
+	 static $has_one = array(
+		'CoverPhoto' => 'Image',
+	 );
 
 
-   function getCMSFields() {
-    $fields = parent::getCMSFields();
-    $fields->addFieldToTab("Root.Content.CoverPhoto", new ImageField('CoverPhoto'));
+	function getCMSFields() {
+		$fields = parent::getCMSFields();
+		$fields->addFieldToTab("Root.Content.CoverPhoto", new ImageField('CoverPhoto'));
 
-    
-    $fields->renameField("Content", "Brief Description");
-   
 
-    /*
-    $fields->addFieldToTab('Root.Content.Main', new CalendarDateField('Date'), 'Content');
-    $fields->addFieldToTab('Root.Content.Main', new TextField('Author'), 'Content');
-    */
-    return $fields;
-  }
-  
+		$fields->renameField("Content", "Brief Description");
+
+
+		/*
+		$fields->addFieldToTab('Root.Content.Main', new CalendarDateField('Date'), 'Content');
+		$fields->addFieldToTab('Root.Content.Main', new TextField('Author'), 'Content');
+		*/
+		return $fields;
+	}
+
 }
- 
+
 class FlickrFolder_Controller extends Page_Controller {
- 
+
 }
- 
-?>
