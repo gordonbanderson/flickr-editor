c765988b7bf435a2a892714ed2793955a27fb470
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-17 19:47:10 +0700

    FIX: Whitespace

--- code/FlickrSetAdmin.php
+++ code/FlickrSetAdmin.php
@@ -2,9 +2,9 @@
 
 class FlickrSetAdmin extends ModelAdmin {
 
-  public static $managed_models = array(   //since 2.3.2
-	  'FlickrSet'
-   );
+	public static $managed_models = array(   //since 2.3.2
+		'FlickrSet'
+	);
 
 	static $url_segment = 'flickr_sets'; // will be linked as /admin/products
 	static $menu_title = 'Flickr Sets';
