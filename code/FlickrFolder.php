f75a6c464daeca9a82ba3ccb8eea8c6dbf569db3
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-18 17:22:13 +0700

    FIX: Don't use now obsolete image field

--- code/FlickrFolder.php
+++ code/FlickrFolder.php
@@ -14,7 +14,7 @@ class FlickrFolder extends Page {
 
 	function getCMSFields() {
 		$fields = parent::getCMSFields();
-		$fields->addFieldToTab("Root.Content.CoverPhoto", new ImageField('CoverPhoto'));
+		$fields->addFieldToTab("Root.Content.CoverPhoto", new UploadField('CoverPhoto'));
 
 
 		$fields->renameField("Content", "Brief Description");
