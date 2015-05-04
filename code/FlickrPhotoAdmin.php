812c9a21e444e24de8f580cdeb03c554bba31cc3
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-05-04 13:40:12 +0700

    ENHANCEMENT: Addition of flickr author

--- code/FlickrPhotoAdmin.php
+++ code/FlickrPhotoAdmin.php
@@ -3,7 +3,8 @@
 class FlickrPhotoAdmin extends ModelAdmin {
 
 	public static $managed_models = array(   //since 2.3.2
-		'FlickrPhoto'
+		'FlickrPhoto',
+		'FlickrAuthor'
 	 );
 
 	static $url_segment = 'flickr_photos'; // will be linked as /admin/products
