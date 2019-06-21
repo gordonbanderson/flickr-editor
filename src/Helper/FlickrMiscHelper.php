aa453466e2c046759a8fe0093e006050fa45fca2
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-06-21 17:01:09 +0100

    FIX: Queries

--- src/Helper/FlickrMiscHelper.php
+++ src/Helper/FlickrMiscHelper.php
@@ -11,6 +11,7 @@ use SilverStripe\ORM\DB;
 use Suilven\Flickr\Model\Flickr\FlickrExif;
 use Suilven\Flickr\Model\Flickr\FlickrPhoto;
 use Suilven\Flickr\Model\Flickr\FlickrSet;
+use Suilven\Flickr\Model\Site\FlickrSetPage;
 
 
 class FlickrMiscHelper extends FlickrHelper
@@ -19,6 +20,10 @@ class FlickrMiscHelper extends FlickrHelper
     {
         $sets = FlickrSet::get()->filter(['PrimaryFlickrPhotoID' => 0]);
         $photosHelper = $this->getPhotoSetsHelper();
+
+        /**
+         * @var FlickrSet $set
+         */
         foreach ($sets as $set) {
             $pageCtr = 1;
             $flickrSetID = $set->FlickrID;
@@ -27,10 +32,13 @@ class FlickrMiscHelper extends FlickrHelper
             $allPagesRead = false;
 
             while (!$allPagesRead) {
+                error_log('Page CTR: ' . $pageCtr);
+                error_log('SET ID: ' . $flickrSetID);
+
                 $photos = $photosHelper->getPhotos(
                     $flickrSetID,
                     null,
-                    'license, date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_m, url_o, url_l,description',
+                    null,
                     500,
                     $pageCtr
                 );
@@ -55,11 +63,9 @@ class FlickrMiscHelper extends FlickrHelper
                         if (isset($fp)) {
                             $set->PrimaryFlickrPhotoID = $fp->ID;
                             $set->write();
-                        } else {
-                            $firstPicID = $set->FlickrPhotos()->first()->ID;
-                            $set->PrimaryFlickrPhotoID = $firstPicID;
-                            $set->write();
                         }
+
+                        // @todo What if there is no primary id set?
                     }
                 }
             }
@@ -69,7 +75,7 @@ class FlickrMiscHelper extends FlickrHelper
 
     public function fixDateSetTaken()
     {
-        $fsps = DataList::create('FlickrSetPage')->where('FirstPictureTakenAt is NULL');
+        $fsps = FlickrSetPage::get()->where(['FirstPictureTakenAt'=> null]);
         foreach ($fsps as $fsp) {
             $fs = $fsp->FlickrSetForPage();
 
