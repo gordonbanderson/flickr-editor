89394b9cea98333e6f657356894e5e26c55eeb8e
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-07-10 18:43:17 +0100

    FIX: Routing config

--- _config.php
+++ _config.php
@@ -10,11 +10,7 @@ use SilverStripe\View\Parsers\ShortcodeParser;
 
 ShortcodeParser::get('default')->register('FlickrPhoto',array('Suilven\Flickr\ShortCode\FlickrPhotoShortCodeHandler','parse_flickr'));
 
-//define global path to Components' root folder
-if(!defined('FLICKR_EDIT_TOOLS_PATH'))
-{
-	define('FLICKR_EDIT_TOOLS_PATH', rtrim(basename(dirname(__FILE__))));
-}
+
 
 // @todo replace with new gis module
 // Object::add_extension('FlickrSet','MapLayerExtension');
