1dcae7f8c360760af3f4a3c0bfbc450f07dbc031
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-07-10 18:46:46 +0100

    ENHANCEMENT: Moving functions to helpers,as FlickrController is well bloated

---
+++ src/Helper/FlickrBatchHelper.php
@@ -0,0 +1,52 @@
+<?php
+namespace Suilven\Flickr\Helper;
+
+use SilverStripe\ORM\DataList;
+use Suilven\Flickr\Model\Flickr\FlickrTag;
+
+class FlickrBatchHelper extends FlickrHelper
+{
+    public function batchUpdateSet($flickrSet, $batchTitle, $batchDescription, $batchTags)
+    {
+        //FIXME authentication
+
+
+        $flickrPhotos = $flickrSet->FlickrPhotos();
+
+        // $batchDescription = $batchDescription ."\n\n".$flickrSet->ImageFooter;
+        // $batchDescription = $batchDescription ."\n\n".$this->SiteConfig()->ImageFooter;
+
+        $tags = array();
+        foreach ($batchTags as $batchTag) {
+            $batchTag = trim($batchTag);
+            $lowerCaseTag = strtolower($batchTag);
+            //$possibleTags = DataList::create('FlickrTag')->where("Value='".$lowerCaseTag."'")
+            $possibleTags = FlickrTag::get()->filter(['Value' => $lowerCaseTag]);
+
+            if ($possibleTags->count() == 0) {
+                $tag = new FlickrTag();
+                $tag->Value = $lowerCaseTag;
+                $tag->RawValue = $batchTag;
+                $tag->write();
+            } else {
+                $tag = $possibleTags->first();
+            }
+
+            array_push($tags, $tag->ID);
+        }
+
+        foreach ($flickrPhotos as $fp) {
+            $fp->Title=$batchTitle;
+            $fp->Description = $batchDescription;
+            $fp->FlickrTags()->addMany($tags);
+            $fp->write();
+        }
+
+        $result = array(
+            'number_of_images_updated' => $flickrPhotos->count()
+        );
+
+        return $result;
+    }
+
+}
---
+++ src/Helper/FlickrBucketHelper.php
@@ -0,0 +1,31 @@
+<?php
+namespace Suilven\Flickr\Helper;
+
+use SilverStripe\ORM\DataObject;
+use Suilven\Flickr\Model\Flickr\FlickrBucket;
+use Suilven\Flickr\Model\Flickr\FlickrPhoto;
+use Suilven\Flickr\Model\Flickr\FlickrSet;
+
+
+class FlickrBucketHelper extends FlickrHelper
+{
+    // @todo DOCS
+    public function createBucket($flickrSetID, $flickrPhotoIDs)
+    {
+        // @todo Check if the ORM does in
+        $flickrPhotos = FlickrPhoto::get()->where('"ID" in ('.$flickrPhotoIDs.')');
+        $flickrSet = DataObject::get_by_id(FlickrSet::class, $flickrSetID);
+        $bucket = new FlickrBucket();
+        $bucket->write();
+
+        $bucketPhotos = $bucket->FlickrPhotos();
+        foreach ($flickrPhotos as $fp) {
+            $bucketPhotos->add($fp);
+        }
+        $bucket->FlickrSetID = $flickrSet->ID;
+        $bucket->write();
+
+        return $bucket;
+    }
+
+}
