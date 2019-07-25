3b5678d4bacb7a382eadb6b9fc25bd25b973b05e
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-07-25 20:41:29 +0100

    WIP: Sprites now being generated

--- src/Task/FlickrSetSpriteTask.php
+++ src/Task/FlickrSetSpriteTask.php
@@ -8,23 +8,11 @@
 
 namespace Suilven\Flickr\Task;
 
-use Samwilson\PhpFlickr\PhotosetsApi;
-use Samwilson\PhpFlickr\PhpFlickr;
-use SilverStripe\Assets\Folder;
-use SilverStripe\Assets\Image;
-use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Control\Director;
-use SilverStripe\Core\Config\Config;
 use SilverStripe\Dev\BuildTask;
-use SilverStripe\ORM\DataObject;
-use SilverStripe\ORM\DB;
 use SilverStripe\Security\Permission;
 use SilverStripe\Security\Security;
-use Suilven\Flickr\Helper\FlickrHelper;
 use Suilven\Flickr\Helper\FlickrSetHelper;
-use Suilven\Flickr\Model\Flickr\FlickrPhoto;
-use Suilven\Flickr\Model\Flickr\FlickrSet;
-use Suilven\Flickr\Model\Site\FlickrSetPage;
 use Suilven\Spriter\Spriter;
 
 
@@ -64,7 +52,7 @@ class FlickrSetSpriteTask extends BuildTask
         $targetDir = 'public/flickr/' . $flickrSetID;
 
         $spriterConfig = [
-            "forceGenerate" => false,                 // set to true if you want to force the CSS and sprite generation.
+            "forceGenerate" => true,                 // set to true if you want to force the CSS and sprite generation.
 
             "srcDirectory" => $targetDir, // folder that contains the source pictures for the sprite.
             "spriteDirectory" => "public/flickr/sprites",   // folder where you want the sprite image file to be saved (folder has to be writable by your webserver)
@@ -73,7 +61,7 @@ class FlickrSetSpriteTask extends BuildTask
             "spriteFilename" => "icon-sprite-" . $flickrSetID,        // name of the generated CSS and PNG file.
 
             "tileMargin" => 0,                        // margin in px between tiles in the highest 'retina' dimension (default is 0) - if you generate different 'retina' dimensions, take a common multiple of the selected variants.
-            "retina" => [2, 1],                  // defines the desired 'retina' dimensions, you want.
+            "retina" => [2,1],                  // defines the desired 'retina' dimensions, you want. [2,1
             "retinaDelimiter" => "@",                 // delimiter inside the sprite image filename.
             "namespace" => "icon-",                   // namespace for your icon CSS classes
 
@@ -84,17 +72,18 @@ class FlickrSetSpriteTask extends BuildTask
                 // you can define multiple targets that will all reference the same png sprite
                 [
                     "cssDirectory" => "public/flickr/sprites",         // folder where you want the sprite CSS to be saved (folder has to be writable, too)
-                    "cssFilename" => "icon-sprite.sass",      // your CSS/Less/Sass target file
-                    "globalTemplate" => "...",                // global template, which contains general CSS styles for all icons (remove line for default)
-                    "eachTemplate" => "...",                  // template for each CSS icon class (remove line for default)
-                    "eachHoverTemplate" => "...",             // template for each CSS icon hover class (remove line for default)
-                    "ratioTemplate" => "..."                  // template for each retina media query (remove line for default)
+                    "cssFilename" => "icon-sprite-" . $flickrSetID .".css",      // your CSS/Less/Sass target file
+                   // "globalTemplate" => "vendor/suilven/php-spriter/src/templates/",                // global template, which contains general CSS styles for all icons (remove line for default)
+                   // "eachTemplate" => "vendor/suilven/php-spriter/src/templates/",                  // template for each CSS icon class (remove line for default)
+                   // "eachHoverTemplate" => "vendor/suilven/php-spriter/src/templates/",             // template for each CSS icon hover class (remove line for default)
+                   // "ratioTemplate" => "vendor/suilven/php-spriter/src/templates/"                  // template for each retina media query (remove line for default)
                 ]
             ]
 
         ];
 
         $spriter = new Spriter($spriterConfig);
+
 
     }
 
