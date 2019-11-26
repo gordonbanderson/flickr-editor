542155af8c46c22fbc411cb8dac060d3cddb6590
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-11-26 00:25:04 +0000

    FIX: Allow for multiple sizes of image

--- src/Extension/FlickrImageURLExtension.php
+++ src/Extension/FlickrImageURLExtension.php
@@ -6,12 +6,36 @@ use Suilven\Flickr\Model\Flickr\FlickrPhoto;
 
 class FlickrImageURLExtension extends Extension
 {
-    public function getFlickrURLFromID($flickrID)
+    public function getFlickrURLFromID($flickrID, $size='l')
     {
         /** @var FlickrPhoto $flickrImage */
         $flickrImage = FlickrPhoto::get()->filter(['FlickrID' => $flickrID])->first();
 
         // @todo configurable size
+        switch($size) {
+            case 's':
+                return $flickrImage->SmallURL;
+            case 'sq':
+                return $flickrImage->SquareURL;
+            case 'sq150':
+                return $flickrImage->SmallURL150;
+            case 'm':
+                return $flickrImage->MediumURL;
+            case 'm640':
+                return $flickrImage->MediumURL640;
+            case 'm800':
+                return $flickrImage->MediumURL800;
+            case 'l':
+                return $flickrImage->LargeURL;
+            case 'l1600':
+                return $flickrImage->LargeURL1600;
+            case 'l2048':
+                return $flickrImage->LargeURL2048;
+            case 'o':
+                return $flickrImage->OriginalURL;
+        }
+
+        // return something if size cannot be determined
         return $flickrImage->LargeURL;
     }
 }
