221c24f7d361b50423909f9c7d71b01c0f51ffdb
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-01-26 22:00:34 +0000

    MINOR: Linting

--- admin/client/src/js/components/app.js
+++ admin/client/src/js/components/app.js
@@ -1,10 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-const t = 'test';
 
 ReactDOM.render(
-	<h1>
-		Hello world!
-	</h1>,
-	document.getElementById('react-visible-image-selecctor')
+  <h1>Hello world!</h1>,
+  document.getElementById('react-visible-image-selecctor'),
 );
