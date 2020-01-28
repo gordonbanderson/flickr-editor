98a94ce9caf6d32ebc6eef6c682deac7495ae77b
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-01-28 19:23:12 +0000

    WIP: Trying to get photo loading to run through redux

--- admin/client/src/js/components/photoActions.js
+++ admin/client/src/js/components/photoActions.js
@@ -1,48 +1,83 @@
 import FlickrPhoto from "./FlickrPhoto";
 import React from "react";
+import {client} from "./transport";
 
 export const FETCH_PHOTOS_BEGIN   = 'FETCH_PHOTOS_BEGIN';
 export const FETCH_PHOTOS_SUCCESS = 'FETCH_PHOTOS_SUCCESS';
 export const FETCH_PHOTOS_FAILURE = 'FETCH_PHOTOS_FAILURE';
 
-export const fetchProductsBegin = () => ({
+export const fetchPhotosBegin = () => ({
 	type: FETCH_PHOTOS_BEGIN
 });
 
-export const fetchProductsSuccess = photos => ({
+export const fetchPhotosSuccess = photos => ({
 	type: FETCH_PHOTOS_SUCCESS,
 	payload: { photos }
 });
 
-export const fetchProductsFailure = error => ({
+export const fetchPhotosFailure = error => ({
 	type: FETCH_PHOTOS_FAILURE,
 	payload: { error }
 });
 
 export function fetchPhotos() {
 	return dispatch => {
-		dispatch(fetchProductsBegin());
-		return fetch("/products")
-			.then(res => res.json())
+		dispatch(fetchPhotosBegin());
+		return client
+			.query({
+				query: gql`
+					  query {
+				  readFlickrSets(ID: ${this.props.ID}) {
+					ID
+					Title
+					FlickrID
+					FlickrPhotos(limit: 100) {
+					  edges {
+						node {
+						  ID
+						  Title
+						  FlickrID
+						  ThumbnailURL
+						}
+					  }
+					  pageInfo {
+							hasNextPage
+							hasPreviousPage
+							totalCount
+						  }
+					}
+				  }
+				}
+					`
+			})
+
+			.then(
+				payload => {
+					var photos = payload.data.readFlickrSets[0].FlickrPhotos.edges
+					dispatch(fetchPhotosSuccess(photos));
+					return photos;
+				}
+				
+			)
 			.then(json => {
-				dispatch(fetchProductsSuccess(json.products));
+				dispatch(fetchPhotosSuccess(json.products));
 				return json.products;
 			})
-			.catch(error => dispatch(fetchProductsFailure(error)));
+			.catch(error => dispatch(fetchPhotosFailure(error)));
 	};
 }
 
 
 export function fetchPhotosORIG() {
 	return dispatch => {
-		dispatch(fetchProductsBegin());
+		dispatch(fetchPhotosBegin());
 		return fetch("/products")
 			.then(res => res.json())
 			.then(json => {
-				dispatch(fetchProductsSuccess(json.products));
+				dispatch(fetchPhotosSuccess(json.products));
 				return json.products;
 			})
-			.catch(error => dispatch(fetchProductsFailure(error)));
+			.catch(error => dispatch(fetchPhotosFailure(error)));
 	};
 }
 
