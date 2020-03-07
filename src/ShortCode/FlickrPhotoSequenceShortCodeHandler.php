7a242b54bc40782719f1198c1420a696aefd9833
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-03-07 23:10:01 +0000

    ENHANCEMENT: An inneficient rendering of filmstrip

--- src/ShortCode/FlickrPhotoSequenceShortCodeHandler.php
+++ src/ShortCode/FlickrPhotoSequenceShortCodeHandler.php
@@ -4,36 +4,60 @@ namespace Suilven\Flickr\ShortCode;
 use SilverStripe\Core\Convert;
 use SilverStripe\ORM\ArrayList;
 use SilverStripe\ORM\DataList;
+use SilverStripe\ORM\DataObject;
 use SilverStripe\View\ArrayData;
 use SilverStripe\View\SSViewer;
 use Suilven\Flickr\Model\Flickr\FlickrPhoto;
+use Suilven\Flickr\Model\Flickr\FlickrSet;
 
 class FlickrPhotoSequenceShortCodeHandler
 {
 
-    // taken from http://www.ssbits.com/tutorials/2010/2-4-using-short-codes-to-embed-a-youtube-video/ and adapted for SS3
     public static function parse_flickr($arguments, $caption = null, $parser = null)
     {
 
         // first things first, if we dont have a image ids, then we don't need to
         // go any further
-        if (empty($arguments['ids'])) {
-            return;
+        if (empty($arguments['setid']) || empty($arguments['photoid']) || empty($arguments['nframes'])) {
+            return '<div style="color: red">Please provide these parameters - setid, photoid, nframes</div>';
         }
 
         $customise = array();
 
         $images = new ArrayList();
-        $idsArray = explode(',', $arguments['ids']);
+        $setID =  $arguments['setid'];
+        $startPhotoID = $arguments['photoid'];
 
-        foreach($idsArray as $id) {
-            $fp = DataList::create(FlickrPhoto::class)->filter(['FlickrID' => $id])->first();
-            error_log('FP: ' . $fp->ID);
-            if (!$fp) {
-                return '';
+        $frames = $arguments['nframes'];
+
+        /** @var FlickrSet $set */
+        $set = FlickrSet::get()->filter('FlickrID', $setID)->first(); // flickr set IDs are unique
+        echo 'SPID=' . $startPhotoID;
+        $startPhoto = $set->FlickrPhotos()->filter('FlickrID', $startPhotoID)->first();
+        //         $startPhoto = FlickrPhoto::get()->filter('FlickrID', $startPhotoID)->first();
+        $sortField = $set->SortOrder;
+
+        echo 'ID=' . $startPhoto->ID;
+
+
+        // @todo Use ORM
+        $ctr = 0;
+        $adding = false;
+       // $frames = 100;
+        foreach ($set->FlickrPhotos()->sort($sortField) as $photo) {
+            if ($ctr > $frames) {
+                break;
             }
-            $images->add($fp);
+
+            if ($photo->FlickrID == $startPhotoID ) {
+                $adding = true;
+            }
+            if ($adding) {
+                $images->add($photo);
+                $ctr++;
+            }
         }
+
 
         $customise['FlickrSequence'] = $images;
         //set the caption
@@ -46,14 +70,8 @@ class FlickrPhotoSequenceShortCodeHandler
         }
 
 
-        $customise['Caption'] = $caption ? Convert::raw2xml($caption) : $fp->Title ;
-        $customise['Position'] = !empty($arguments['position']) ? $arguments['position'] : 'center';
-        $customise['Small'] = true;
-        if ($customise['Position'] == 'center') {
-            $customise['Small'] = false;
-        }
+        $customise['Caption'] = $caption ? Convert::raw2xml($caption) : $startPhoto->Title ;
 
-        $fp = null;
 
         //overide the defaults with the arguments supplied
         $customise = array_merge($customise, $arguments);
