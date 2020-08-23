52fabcfdacd7b3f9cafdec49e2f2ce46ad6eb349
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-08-23 15:38:05 +0100

    FIX: PHPStan now runs

--- src/GridFieldExifData.php
+++ src/GridFieldExifData.php
@@ -2,6 +2,7 @@
 
 namespace Suilven\Flickr;
 
+use SilverStripe\Forms\GridField\GridField;
 use SilverStripe\Forms\GridField\GridField_HTMLProvider;
 use SilverStripe\View\ArrayData;
 
@@ -16,8 +17,25 @@ use SilverStripe\View\ArrayData;
  */
 class GridFieldExifData implements GridField_HTMLProvider
 {
-    /** @return array<string,string> */
-    public function getHTMLFragments(\SilverStripe\Forms\GridField\GridField $gridField): array
+    /**
+     * Returns a map where the keys are fragment names and the values are
+     * pieces of HTML to add to these fragments.
+     *
+     * Here are 4 built-in fragments: 'header', 'footer', 'before', and
+     * 'after', but components may also specify fragments of their own.
+     *
+     * To specify a new fragment, specify a new fragment by including the
+     * text "$DefineFragment(fragmentname)" in the HTML that you return.
+     *
+     * Fragment names should only contain alphanumerics, -, and _.
+     *
+     * If you attempt to return HTML for a fragment that doesn't exist, an
+     * exception will be thrown when the {@link GridField} is rendered.
+     *
+     * @param GridField $gridField
+     * @return array
+     */
+    public function getHTMLFragments($gridField)
     {
         $forTemplate = new ArrayData([]);
 
--- src/ModelAdmin/FlickrPhotoAdmin.php
+++ src/ModelAdmin/FlickrPhotoAdmin.php
@@ -3,6 +3,7 @@
 namespace Suilven\Flickr\ModelAdmin;
 
 use SilverStripe\Admin\ModelAdmin;
+use SilverStripe\ORM\DataObject;
 use Suilven\Flickr\Model\Flickr\FlickrAuthor;
 use Suilven\Flickr\Model\Flickr\FlickrPhoto;
 
@@ -11,14 +12,21 @@ use Suilven\Flickr\Model\Flickr\FlickrPhoto;
  */
 class FlickrPhotoAdmin extends ModelAdmin
 {
+    /**
+     * @var array<string>
+     */
     private static $managed_models = [
         FlickrPhoto::class,
         FlickrAuthor::class,
      ];
 
-    // will be linked as /admin/products
+    /** @var string  */
     private static $url_segment = 'flickr_photos';
+
+    /** @var string  */
     private static $menu_title = 'Flickr Photos';
 
+    // @todo Check if I moved this over to Suilven namespace
+    /** @var string  */
     private static $menu_icon = 'weboftalent/flickr:icons/photo.png';
 }
